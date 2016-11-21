3a0ffb21f190688aa6abf95dd70c21b103a76abd
Justin Chines <justin.chines@gmail.com>
2016-11-21 02:43:35 -0500

    Big userscript cleanup
    - Fixed Chrome support
    - Removed dependence on Youtube Center for embedded videos

--- Video_Auto-downloader.user.js
+++ Video_Auto-downloader.user.js
@@ -1,94 +1,89 @@
 // ==UserScript==
 // @name        Video Auto-downloader
 // @namespace   png-vid-archive
-// @description Downloads every viewed youtube video
+// @description Downloads every viewed online video
 // @include     http*://*youtube.com/watch?v=*
 // @include     http*://*youtube.com/embed/*
 // @include     http*://vimeo.com/*
 // @version     1.33.7
-// @grant       unsafeWindow
-// @grant       GM_xmlhttpRequest
-// @permissions http://ssh.locker.phinugamma.org/
 // ==/UserScript==
 
-/* XXX TODO
- To remove dependence on ytcenter,
- we gotta hook into onYoutubePlayerReady,
- and call any other callbacks if they exist,
- and save the api for ourselves.
- (This is how ytcenter does it (y) */
+function inject(func) {
+  // Injects a script into the content page.
+  // Needed to escape Chrome sandbox
+  var script = document.createElement("script");
+  script.setAttribute("type", "text/javascript");
+  script.appendChild(document.createTextNode("(" + func + ")();"));
 
-YOUTUBE_ARCHIVER_HOST = "locker.phinugamma.org"
-
-function sendPage(uri) {
-  GM_xmlhttpRequest({
-    method: 'GET',
-    url: '//' + YOUTUBE_ARCHIVER_HOST + '/dlvid?url=' + escape(uri)
-  });
-  
-  // Old XHR code. Used to work... even though it shouldn't have
-  // because of cross-origin issues
-  // Unsafe window shit used to magically work too. ~WEIRD~
-  //var xhr = new XMLHttpRequest();
-  //xhr.open('GET', 'http://localhost:8080/dlvid?url=' + escape(uri));
-  //xhr.send();
-  
-  console.log("[Auto-download] Sent request for " + uri);
+  document.body.appendChild(script);
+  document.body.removeChild(script);
 }
 
-function getURLFromPlayer(player) {
-  var currentVid = player.getVideoUrl();
-  
-  // Remove stupid timestamp
-  currentVid = currentVid.replace(/t=[0-9]*&/, "");
-  
-  return currentVid;
-}
+function YoutubeAutoDownloader() {
+  // This fucntion gets injected into the page and executed
+  var YOUTUBE_ARCHIVER_HOST = "locker.phinugamma.org";
+  var currentUrl;
 
-
-// Entry point //
-console.log("[Auto-download] Youtube Auto-dowloader loading");
-
-if (window.top == window.self) {
-  var currentPage = undefined
-
-  function checkNavigate() {
-    // Check every second for page change
-    if (unsafeWindow.document.location.toString() != currentPage) {
-      console.log("[Auto-download] Sending Youtube page");
-      currentPage = unsafeWindow.document.location.toString()
-      sendPage(currentPage);
-    }
-    setTimeout(checkNavigate, 2000)
+  function sendPage(uri) {
+    // Fuck your same-origin policy :smug:
+    var image = new Image();
+    image.src = '//' + YOUTUBE_ARCHIVER_HOST + '/dlvid?url=' + escape(uri);
+    console.log("[Auto-download] Sent request for " + uri);
   }
-  checkNavigate()
-} else if (window.location.toString().search('/embed/') > 0) {
-  var player;
-  var currentVid;
-  var pageSent = false;
-  
-  console.log("[Auto-download] Detected embed");
-  
-  // Delay 1 second for api to ready up
-  setTimeout(function () {
-    if (!player) player = unsafeWindow.ytcenter.player.getAPI();
-    currentVid = getURLFromPlayer(player)
-    console.log("[Auto-download] Embed video detected: " + currentVid);
-  
-    var id = setInterval(function () { 
-      // Send that shit if we watch it
-      if (!pageSent && player.getPlayerState() == 1) {
-        sendPage(currentVid);
-        pageSent = true;
-      }
 
-      // Handle changed vid
-      var newURL = getURLFromPlayer(player);
-      if (currentVid != newURL) {
-        currentVid = newURL;
-        pageSent = false;
+  function checkPageNavigate() {
+    // Poll for page changes. Needed since youtube will dynamically load pages now.
+    locationStr = document.location.toString();
+    if (locationStr != currentUrl) {
+      console.log("[Auto-download] Sending Youtube page");
+      currentUrl = locationStr;
+      sendPage(currentUrl);
+    }
+    setTimeout(checkPageNavigate, 2000)
+  }
+
+  function checkEmbedReadiness() {
+    if (window.yt) {
+      console.log("[Auto-download] Setting up embedded handler")
+      setupEmbeddedEventHandler();
+    } else {
+      console.log("[Auto-download] Embedded player not ready. Trying again...")
+      setTimeout(checkEmbedReadiness, 1000);
+    }
+  }
+
+  function getURLFromPlayer(player) {
+    // Returns a clean URL from the player
+    // Need to remove timestamp that embedded player tries to add
+    return player.getVideoUrl().replace(/t=[0-9]*&/, "");
+  }
+
+  function setupEmbeddedEventHandler() {
+    // Hook into embedded player event
+    player = window.yt.player.getPlayerByElement("player");
+    player.addEventListener('onStateChange', function(state) {
+      if (state == 1) { // Playing
+        var playingVid = getURLFromPlayer(player);
+        if (playingVid != currentUrl) {
+          currentUrl = playingVid;
+          sendPage(currentUrl);
+        }
       }
-      
-    }, 1000);
-  }, 1000);
+    });
+  } 
+
+  if (window.top == window.self) {
+    // This is a youtube page. Start the checker to look for page changes, and
+    // send archive commands as needed.
+    console.log("[Auto-download] Detected video page");
+    checkPageNavigate();
+  } else if (window.location.toString().search('/embed/') > 0) {  
+    // This is a youtube embed. Need to hook up event handler
+    console.log("[Auto-download] Detected embed");
+    checkEmbedReadiness();
+  }
 }
+
+// o shit waddup
+console.log("[Auto-download] Youtube Auto-dowloader loading");
+inject(YoutubeAutoDownloader);
--- start-server.vbs
+++ start-server.vbs
@@ -1,2 +1,3 @@
+'Simple util script for running dl script in background on Windows
 Set objShell = WScript.CreateObject("WScript.Shell")
 objShell.Run("dl.py"), 0, True
